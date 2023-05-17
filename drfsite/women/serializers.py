36b91edba77191f4fa3c1ea874a5d6a793441a8c
Muslix64st <1hpp48@gmail.com>
2023-05-17 10:12:24 +0300

    serializers test 3 update DB

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,26 +1,29 @@
 import io
-
-from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-
 from .models import *
 
-
-
-# class WomenModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
+from rest_framework import serializers
 
 
 class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
-    time_create = serializers.DateTimeField()
-    time_update = serializers.DateTimeField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        return instance.update(**validated_data)
+#---------------------------------------------------------------------------------------------
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
 #
 # def encode():
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,7 +1,7 @@
-from django.forms import model_to_dict
-from rest_framework import generics, status
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+# from django.forms import model_to_dict
+# from rest_framework import generics, status
+# from django.shortcuts import render
+# from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
@@ -10,31 +10,10 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        women = Women.objects.all()
-        # serializer = WomenSerializer(women, many=True)
-        return Response(WomenSerializer(women, many=True).data)
-
-        # lst = Women.objects.all().values()
-        # return Response({'post get': list(lst)})
-
+        return Response(WomenSerializer(Women.objects.all(), many=True).data)
 
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id'],
-        )
-        return Response({'post': WomenSerializer(post_new).data})
-
-
-    # def put(self, request):
-
-
-
-
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+        serializer.save()
+        return Response({'post': serializer.data})
